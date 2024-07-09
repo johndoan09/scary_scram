212e7440d1061cd7123acb114114c084c866dab2
johndoan09 <johndoaneo@gmail.com>
2024-07-09 18:28:58 -0400

    added sounds

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 - walls: canva.com
 - top view door: https://www.vecteezy.com/free-png/white-rectangle
 - door: https://www.pinterest.com/pin/656188608198504388/
+- heart beat sound: https://freesound.org/people/patobottos/sounds/369017/
+- scary sound: https://freesound.org/people/tyops/sounds/393575/
+- jump scare sound: https://freesound.org/people/tyops/sounds/448204/
--- script.js
+++ script.js
@@ -3,8 +3,11 @@ let myanswer = 0;
 let yourpick = 0;
 let youranswer = 0;
 let stage = 3;
+let soundOn = false;
+
 
 document.addEventListener('DOMContentLoaded', () => {
+    
     const doorone = document.getElementById('doorone');
     const doortwo = document.getElementById('doortwo');
     const doorthree = document.getElementById('doorthree');
@@ -22,60 +25,126 @@ document.addEventListener('DOMContentLoaded', () => {
     const winbtn = document.getElementById('winbtn');
     const rightscene = document.getElementById('rightscene');
     const wrongscene = document.getElementById('wrongscene');
+    const selectbtn = document.getElementById('selectbtn');
+    const tomselectbtn = document.getElementById('tomselectbtn');
+    const scarySound = document.getElementById('scarySound');
+    const heartSound = document.getElementById('heartbeat');
+    const jumpScare = document.getElementById('jumpscare');
+    const soundbtn = document.getElementById('soundbtn');
 
     scene1.style.display ='flex';
     scene2.style.display = 'none';
+
+    soundbtn.addEventListener('click', () => {
+        if (soundOn) {
+            soundbtn.innerHTML = `Sound: Off`;
+            scarySound.pause();
+            soundOn = false;
+        } else {
+            soundbtn.innerHTML = `Sound: On`;
+            scarySound.load();
+            scarySound.play();
+            soundOn = true;
+        }
+    });
 
     startBtn.addEventListener('click', () => {
         document.getElementById('scene2').style.display = 'flex';
         document.getElementById('scene1').style.display = 'none';
         updateStage();
+        clearDoor();
+        if (soundOn) {
+            scarySound.play();
+        }
     });
 
     rulesBtn.addEventListener('click', () => {
+        if (soundOn) {
+            scarySound.play();
+        }
         document.getElementById('scene1').style.display = 'none';
         document.getElementById('rulescene').style.display = 'flex';
     });
 
     rulesceneBtn.addEventListener('click', () => {
+        if (soundOn) {
+            scarySound.play();
+        }
         document.getElementById('rulescene').style.display = 'none';
         document.getElementById('scene1').style.display = 'flex';
     });
 
-    doorone.addEventListener('click', () => openDoor(1));
-    doortwo.addEventListener('click', () => openDoor(2));
-    doorthree.addEventListener('click', () => openDoor(3));
+    doorone.addEventListener('click', () => openDoor(1, doorone));
+    doortwo.addEventListener('click', () => openDoor(2, doortwo));
+    doorthree.addEventListener('click', () => openDoor(3, doorthree));
 
-    const openDoor = (doorNumber) => {
+    const openDoor = (doorNumber, doorElement) => {
+        clearDoor();
         updateStage();
-        document.getElementById('scene2').style.display = 'none';
-        document.getElementById('darkscene').style.display = 'flex';
+        document.getElementById('selectbtn').style.display = 'flex';
+        doorElement.classList.add('selected');
         mypick = doorNumber;
         myanswer = Math.floor(Math.random() * 3) + 1;
     };
+
+    const clearDoor = () => {
+        doorone.classList.remove('selected');
+        doortwo.classList.remove('selected');
+        doorthree.classList.remove('selected');
+        topone.classList.remove('selected2');
+        toptwo.classList.remove('selected2');
+        topthree.classList.remove('selected2');
+    }
+
+    selectbtn.addEventListener('click', () => {
+        document.getElementById('scene2').style.display = 'none';
+        document.getElementById('darkscene').style.display = 'flex';
+        document.getElementById('soundbtn').style.display = 'none';
+        clearDoor();
+        const randomDelay = Math.floor(Math.random() * 4000) + 3000;
+        scarySound.pause();
+        if (soundOn) {
+            heartSound.play();
+        }
+        setTimeout(() => {
+            document.getElementById('darkscene').style.display = 'none';
+            heartSound.pause();
+            if (mypick == myanswer) {
+                document.getElementById('scaryscene').style.display = 'flex';
+                updateStage();
+                if (soundOn) {
+                    jumpScare.load();
+                    jumpScare.play();
+                }
+            } else {
+                document.getElementById('soundbtn').style.display = 'flex';
+                stage++;
+                updateStage();
+                if (soundOn) {
+                    scarySound.load();
+                    scarySound.play();
+                }
+                if (stage == 6) {
+                    document.getElementById('winscene').style.display = 'flex';
+                } else {
+                    document.getElementById('safescene').style.display = 'flex';
+                }
+            }
+        }, randomDelay);
+    });
 
     const updateStage = () => {
         document.getElementById('stageTracker').innerHTML = `Stage: ${stage}/5`; 
         document.getElementById('stageTracker2').innerHTML = `Stage: ${stage}/5`;
     }
 
-    darkness.addEventListener('click', () => {
-        document.getElementById('darkscene').style.display = 'none';
-        if (mypick == myanswer) {
-            document.getElementById('scaryscene').style.display = 'flex';
-            updateStage();
-        } else {
-            stage++;
-            updateStage();
-            if (stage == 6) {
-                document.getElementById('winscene').style.display = 'flex';
-            } else {
-                document.getElementById('safescene').style.display = 'flex';
-            }
-        }
-    });
-
     scaryscene.addEventListener('click', () => {
+        if (soundOn) {
+            jumpScare.pause();
+            scarySound.load();
+            scarySound.play();
+        }
+        document.getElementById('soundbtn').style.display = 'flex';
         document.getElementById('scaryscene').style.display = 'none';
         document.getElementById('tomscene').style.display = 'flex';
         if (stage == 1) {
@@ -85,15 +154,25 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    topone.addEventListener('click', () => hideDoor(1));
-    toptwo.addEventListener('click', () => hideDoor(2));
-    topthree.addEventListener('click', () => hideDoor(3));
+    topone.addEventListener('click', () => hideDoor(1, topone));
+    toptwo.addEventListener('click', () => hideDoor(2, toptwo));
+    topthree.addEventListener('click', () => hideDoor(3, topthree));
 
-    const hideDoor = (doorNum) => {
+    const hideDoor = (doorNum, doorElement) => {
+        clearDoor();
         updateStage();
-        document.getElementById('tomscene').style.display = 'none';
+        doorElement.classList.add('selected2');
+        document.getElementById('tomselectbtn').style.display = 'flex';
         yourpick = doorNum;
         youranswer = Math.floor(Math.random() * 3) + 1;
+    }
+
+    tomselectbtn.addEventListener('click', () => {
+        if (soundOn) {
+            scarySound.play();
+        }
+        clearDoor();
+        document.getElementById('tomscene').style.display = 'none';
         if (yourpick == youranswer) {
             document.getElementById('rightscene').style.display = 'flex';
             updateStage();
@@ -106,9 +185,13 @@ document.addEventListener('DOMContentLoaded', () => {
                 document.getElementById('wrongscene').style.display = 'flex';
             }
         }
-    }
+    });
 
     rightscene.addEventListener('click', () => {
+        if (soundOn) {
+            scarySound.play();
+        }
+        clearDoor();
         document.getElementById('rightscene').style.display = 'none';
         document.getElementById('scene2').style.display = 'flex';
         if (stage == 5) {
@@ -119,6 +202,10 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     wrongscene.addEventListener('click', () => {
+        if (soundOn) {
+            scarySound.play();
+        }
+        clearDoor();
         document.getElementById('wrongscene').style.display = 'none';
         document.getElementById('tomscene').style.display = 'flex';
         if (stage == 1) {
@@ -129,6 +216,9 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     safescene.addEventListener('click', () => {
+        if (soundOn) {
+            scarySound.play();
+        }
         document.getElementById('safescene').style.display = 'none';
         document.getElementById('scene2').style.display = 'flex';
         if (stage == 5) {
@@ -139,6 +229,10 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     losebtn.addEventListener('click', () => {
+        if (soundOn) {
+            scarySound.load();
+            scarySound.play();
+        }
         document.getElementById('losescene').style.display = 'none';
         document.getElementById('scene1').style.display = 'flex';
         stage = 3;
@@ -147,6 +241,10 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     winbtn.addEventListener('click', () => {
+        if (soundOn) {
+            scarySound.load();
+            scarySound.play();
+        }
         document.getElementById('winscene').style.display = 'none';
         document.getElementById('scene1').style.display = 'flex';
         stage = 3;
